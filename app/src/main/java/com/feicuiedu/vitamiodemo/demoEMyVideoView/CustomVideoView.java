e069768c3687aebf789c40b045ec8881e48b7c77
yuanchao <yuanchao@feicuiedu.com>
2016-08-09 11:30:11 +0800

    更新了一下粗糙的VideoView

--- app/src/main/java/com/feicuiedu/vitamiodemo/demoEMyVideoView/CustomVideoView.java
+++ app/src/main/java/com/feicuiedu/vitamiodemo/demoEMyVideoView/CustomVideoView.java
@@ -9,23 +9,53 @@ import android.view.SurfaceView;
 import android.widget.RelativeLayout;
 
 import com.feicuiedu.vitamiodemo.R;
+import com.feicuiedu.vitamiodemo.VideoUrlRes;
 
 import java.io.IOException;
 
 /**
- * CustomVideoView，使用MediaPlayer + SurfaceView实现视频播放
- * <p/>
- * 在使用此控件前，请先设置视频地址
+ * 一个能播放视频的控件,SurfaceView来显示视频,用MediaPlayer来进行视频的播放
  * <p/>
  * 作者：yuanchao on 2016/8/9 0009 10:00
  * 邮箱：yuanchao@feicuiedu.com
  */
-public class CustomVideoView extends RelativeLayout implements SurfaceHolder.Callback {
-
-    private MediaPlayer mediaPlayer; // 用来播放
-    private SurfaceView surfaceView; // 用来显示播放效果
-    private String videoPath; // 视频地址
+public class CustomVideoView extends RelativeLayout implements SurfaceHolder.Callback{
+    private MediaPlayer mediaPlayer;
+    private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
+
+    // MediaPlayer
+    // MediaPlayer mediaPlayer = new MediaPlayer();
+    // mediaPlayer.setDataSource(数据源);
+    // mediaPlayer.prepare();
+    // mediaPlayer.start()
+    // 但是：MediaPlayer并不能显示视频
+    // mediaPlayer.setDisplay()
+
+    // SurfaceView
+    // SurfaceView 上有一个Surface进行绘制处理,怎么样才能拿到这个Surface，怎么才能将内容显示到Surface上
+    // 它提供了一个        SurfaceHodler
+    // SurfaceView - getVolder();
+
+//        SurfaceView view = null;
+//        SurfaceHolder holder = view.getHolder();
+//        视频画面处理 --- 不用我们去做
+//
+//        mediaPlayer.setDisplay(holder)
+
+    //        Canvas canvas = holder.lockCanvas();
+//        canvas.drawARGB(2,2,2,2);
+//        canvas.drawARGB(2,2,2,2);
+//        canvas.drawARGB(2,2,2,2);
+//        canvas.drawARGB(2,2,2,2);
+//        canvas.drawARGB(2,2,2,2);
+//        canvas.drawARGB(2,2,2,2);
+//        holder.unlockCanvasAndPost(canvas);
+
+
+    // 你只是放了一个SurfaceView的控件,但是，他内部是通过Surface去处理的
+    // 你怎么知道这个Surface有没有创建好？有没有改变大小？有没有dest..???
+    // 所以它提供了一个Callback
 
     public CustomVideoView(Context context) {
         this(context, null);
@@ -37,26 +67,19 @@ public class CustomVideoView extends RelativeLayout implements SurfaceHolder.Cal
 
     public CustomVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        // 当前布局(就是一个SurfaceView)
-        // 一般的需求:
-        // 有SurfaceView（用来显示视频内容）
-        // 有视频控制的控件(比如，播放.......)
-        LayoutInflater.from(getContext()).inflate(R.layout.view_simple_video_player, this, true);
+        // 当前视图（其实目前也就是只有一个SurfaceView，后期当然会加上更多的视频相关控制的控件上去）
+        LayoutInflater.from(context).inflate(R.layout.view_simple_video_player, this, true);
         surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
-    }
-
-    public void setVideoPath(String videoPath) {
-        this.videoPath = videoPath;
     }
 
     @Override public void surfaceCreated(SurfaceHolder holder) {
         try {
             mediaPlayer = new MediaPlayer();
             mediaPlayer.setDisplay(holder);
-            mediaPlayer.setDataSource(videoPath);
-            mediaPlayer.prepareAsync();
+            mediaPlayer.setDataSource(VideoUrlRes.getTestVideo1());
+            mediaPlayer.prepareAsync(); // 异步
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override public void onPrepared(MediaPlayer mp) {
                     mediaPlayer.start();
@@ -68,8 +91,10 @@ public class CustomVideoView extends RelativeLayout implements SurfaceHolder.Cal
     }
 
     @Override public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
     }
 
     @Override public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
--- app/src/main/java/com/feicuiedu/vitamiodemo/demoEMyVideoView/CustomVideoViewActivity.java
+++ app/src/main/java/com/feicuiedu/vitamiodemo/demoEMyVideoView/CustomVideoViewActivity.java
@@ -4,18 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.feicuiedu.vitamiodemo.R;
-import com.feicuiedu.vitamiodemo.VideoUrlRes;
 
 public class CustomVideoViewActivity extends AppCompatActivity {
-
-    private CustomVideoView customVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_video_view);
-
-        customVideoView = (CustomVideoView) findViewById(R.id.customVideoView);
-        customVideoView.setVideoPath(VideoUrlRes.getTestVideo1());
     }
 }
